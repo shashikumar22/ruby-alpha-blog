b03066fcd526651623928d6f048060afc9e2b2d0
shashikumar <shashikumar2203@gmail.com>
2018-03-19 17:05:23 +0530

    list db

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord 
-	validates :title, presence: true, length: {minimum: 10, maximum: 20} 
-	validates :description, presence: true, length: {minimum: 10, maximun:20}
+	validates :title, presence: true, length: {minimum: 3, maximum: 20} 
+	validates :description, presence: true, length: {minimum: 10, maximun:300}
 end
