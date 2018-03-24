b67c50ec5792228d92e7939b634e9962f9fd4e79
shashikumar <shashikumar2203@gmail.com>
2018-03-24 17:47:40 +0530

    pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 	def gravatar_for(user, options = { size: 80})
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		size = options[:size]
-		byebug
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		image_tag(gravatar_url, alt: user.username, class: "img-circle")
 	end
